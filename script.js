a6d6bf57d662372c7d81fead133bf4f65a200362
Solomon Sitotaw <50806926+SolomonSitotaw23@users.noreply.github.com>
2022-02-14 16:48:54 +0300

    fetching from api and displaying done last commit

---
+++ script.js
@@ -0,0 +1,74 @@
+const imageContainer = document.getElementById("image-container");
+const loader = document.getElementById("loader");
+let photosArray = [];
+
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
+// Unsplash API
+const count = 30;
+const apiKey = "gQ_2y-5pN0wV_4fxA6pju5E6rSbXeeF61Fjt0aEWH8M";
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+//Image Loaded Function
+
+function imageLoaded() {
+  imagesLoaded++;
+  if (imagesLoaded === totalImages) {
+    ready = true;
+    loader.hidden = true;
+  }
+}
+
+// Function To set attributes
+
+function setAttribute(element, attributes) {
+  for (const key in attributes) {
+    element.setAttribute(key, attributes[key]);
+  }
+}
+
+function displayPhotos() {
+  imagesLoaded = 0;
+  totalImages = photosArray.length;
+  photosArray.forEach((photo) => {
+    const item = document.createElement("a");
+    setAttribute(item, {
+      href: photo.links.html,
+      target: "_blank",
+    });
+    const img = document.createElement("img");
+    setAttribute(img, {
+      src: photo.urls.regular,
+      alt: photo.alt_description,
+      title: photo.alt_description,
+    });
+    //Event Listener to check if each image is loaded
+    img.addEventListener("load", imageLoaded);
+
+    item.appendChild(img);
+    imageContainer.appendChild(item);
+  });
+}
+
+// Fetch request
+async function getPhotos() {
+  try {
+    const response = await fetch(apiUrl);
+    photosArray = await response.json();
+    displayPhotos();
+  } catch (e) {}
+}
+
+// check to see if scrolling near bottom of the page,and load more photos
+
+window.addEventListener("scroll", () => {
+  if (
+    window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 &&
+    ready
+  ) {
+    getPhotos();
+  }
+});
+
+// on load
+getPhotos();
